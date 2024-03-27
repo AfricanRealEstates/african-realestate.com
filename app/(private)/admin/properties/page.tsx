79e07653595fe9c86ea85b9ea37e9d4a6acae1bb
AfricanRealEstates <martinmungaik@gmail.com>
2024-03-27 19:00:27 +0300

    auth-options

--- app/(private)/admin/properties/page.tsx
+++ app/(private)/admin/properties/page.tsx
@@ -1,9 +1,26 @@
 import Filters from "@/components/globals/filters";
 import PageTitle from "@/components/globals/page-title";
 import PropertiesTable from "@/components/properties/properties-table";
+import { authOptions } from "@/lib/auth-options";
+import { getServerSession } from "next-auth";
 import React from "react";
 
-export default function Properties({ searchParams }: { searchParams: any }) {
+export default async function Properties({
+  searchParams,
+}: {
+  searchParams: any;
+}) {
+  const session = await getServerSession(authOptions);
+
+  if (session?.user.role !== "ADMIN") {
+    return (
+      <section className="py-24">
+        <div className="container">
+          <h2>You are not authorized to view this page</h2>
+        </div>
+      </section>
+    );
+  }
   return (
     <div className="w-[95%] lg:max-w-7xl mx-auto py-[90px] lg:py-[120px]">
       <PageTitle title="Admin / Properties" />
