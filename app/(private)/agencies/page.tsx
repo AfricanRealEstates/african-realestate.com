d2ea6589556f76856d00db4c26b326913d8ad5c6
AfricanRealEstates <martinmungaik@gmail.com>
2024-04-22 22:14:37 +0300

    contact agent

--- app/(private)/agencies/page.tsx
+++ app/(private)/agencies/page.tsx
@@ -75,7 +75,7 @@ export default async function Agencies() {
               <div>
                 <div className="-mt-px flex divide-x divide-gray-200">
                   <div className="flex w-0 flex-1">
-                    <a
+                    <Link
                       href={`mailto:${agent.email}`}
                       className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
                     >
@@ -84,7 +84,7 @@ export default async function Agencies() {
                         aria-hidden="true"
                       />
                       Email
-                    </a>
+                    </Link>
                   </div>
                   <div className="-ml-px flex w-0 flex-1 bg-neutral-50 text-indigo-500 hover:bg-indigo-600 hover:text-neutral-50 transition-colors ease-linear">
                     <Link
