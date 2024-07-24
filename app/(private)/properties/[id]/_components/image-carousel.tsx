d168d1d43e0a6bdfe54d43863df7ef439d1344a2
AfricanRealEstates <martinmungaik@gmail.com>
2024-07-24 19:57:41 +0300

    image carousel

--- app/(private)/properties/[id]/_components/image-carousel.tsx
+++ app/(private)/properties/[id]/_components/image-carousel.tsx
@@ -73,95 +73,93 @@ export default function ImageCarousel({ property }: Props) {
   return (
     <section className="lg:w-3/5 xl:w-2/3 space-y-8 lg:space-y-10 lg:pr-10">
       <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
-        <div className="h-full" onDoubleClick={handleDoubleClick}>
-          <div className="mx-auto flex h-full flex-col justify-center relative">
-            <div className="overflow-hidden relative h-full w-full">
-              <motion.div
-                animate={{ x: -index * 100 + "%" }}
-                className="flex h-full rounded-xl"
-                transition={{ duration: 0.5 }}
-              >
-                {allImages.map((image, i) => (
-                  <div key={i} className="w-full h-full relative">
-                    <Image
-                      src={image}
-                      alt="Property Image"
-                      layout="fill"
-                      objectFit="cover"
-                      className="rounded-xl"
-                    />
-                  </div>
-                ))}
-              </motion.div>
-
-              <AnimatePresence initial={false}>
-                {index > 0 && (
-                  <motion.button
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 0.7 }}
-                    exit={{ opacity: 0, pointerEvents: "none" }}
-                    whileHover={{ opacity: 1 }}
-                    className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
-                    onClick={() => handleChangeIndex(index - 1)}
-                  >
-                    <ChevronLeftIcon className="h-6 w-6" />
-                  </motion.button>
-                )}
-              </AnimatePresence>
-
-              <AnimatePresence initial={false}>
-                {index + 1 < allImages.length && (
-                  <motion.button
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 0.7 }}
-                    exit={{ opacity: 0, pointerEvents: "none" }}
-                    whileHover={{ opacity: 1 }}
-                    className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
-                    onClick={() => handleChangeIndex(index + 1)}
-                  >
-                    <ChevronRightIcon className="h-6 w-6" />
-                  </motion.button>
-                )}
-              </AnimatePresence>
-            </div>
-
-            <div className="absolute bottom-0 inset-x-0 h-20 bg-gradient-to-t from-black opacity-50 rounded-b-lg"></div>
-
-            <div className="h-8 min-w-fit rounded-sm px-2 absolute top-3 right-3 flex cursor-pointer items-center justify-center gap-2 bg-white font-medium leading-6">
-              {index + 1}/{allImages.length} Photos
-            </div>
-
-            <section
-              ref={thumbnailRef}
-              className="w-full absolute bottom-0 left-0 p-2 px-4 rounded-md shadow-md overflow-x-scroll text-xs scrollbar-hide"
+        <div
+          className="relative overflow-hidden h-full w-full"
+          onDoubleClick={handleDoubleClick}
+        >
+          <div className="relative h-full w-full">
+            <motion.div
+              animate={{ x: -index * 100 + "%" }}
+              className="flex h-full w-full"
+              transition={{ duration: 0.5 }}
             >
-              <div className="flex gap-4 w-max">
-                {allImages.map((image, i) => (
-                  <div key={i} className="relative w-16 h-16">
-                    <Image
-                      src={image}
-                      alt="Thumbnail"
-                      layout="fill"
-                      objectFit="cover"
-                      className={`w-full h-full rounded-full ring-2 ring-gray-100 cursor-pointer ${
-                        i === index ? "border-4 border-blue-500" : ""
-                      }`}
-                      onClick={() => handleThumbnailClick(i)}
-                    />
-                  </div>
-                ))}
-              </div>
-            </section>
+              {allImages.map((image, i) => (
+                <div
+                  key={i}
+                  className="relative flex-shrink-0 w-full h-full"
+                  style={{ flexBasis: "100%" }}
+                >
+                  <Image
+                    src={image}
+                    alt={`Property Image ${i}`}
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-xl"
+                  />
+                </div>
+              ))}
+            </motion.div>
+
+            <AnimatePresence initial={false}>
+              {index > 0 && (
+                <motion.button
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 0.7 }}
+                  exit={{ opacity: 0, pointerEvents: "none" }}
+                  whileHover={{ opacity: 1 }}
+                  className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
+                  onClick={() => handleChangeIndex(index - 1)}
+                >
+                  <ChevronLeftIcon className="h-6 w-6" />
+                </motion.button>
+              )}
+            </AnimatePresence>
+
+            <AnimatePresence initial={false}>
+              {index + 1 < allImages.length && (
+                <motion.button
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 0.7 }}
+                  exit={{ opacity: 0, pointerEvents: "none" }}
+                  whileHover={{ opacity: 1 }}
+                  className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
+                  onClick={() => handleChangeIndex(index + 1)}
+                >
+                  <ChevronRightIcon className="h-6 w-6" />
+                </motion.button>
+              )}
+            </AnimatePresence>
           </div>
+
+          <div className="absolute bottom-0 inset-x-0 h-20 bg-gradient-to-t from-black opacity-50 rounded-b-lg"></div>
+
+          <div className="h-8 min-w-fit rounded-sm px-2 absolute top-3 right-3 flex cursor-pointer items-center justify-center gap-2 bg-white font-medium leading-6">
+            {index + 1}/{allImages.length} Photos
+          </div>
+
+          <section
+            ref={thumbnailRef}
+            className="w-full absolute bottom-0 left-0 p-2 px-4 rounded-md shadow-md overflow-x-scroll text-xs scrollbar-hide"
+          >
+            <div className="flex gap-4 w-max">
+              {allImages.map((image, i) => (
+                <div key={i} className="relative w-16 h-16">
+                  <Image
+                    src={image}
+                    alt="Thumbnail"
+                    layout="fill"
+                    objectFit="cover"
+                    className={`w-full h-full rounded-full ring-2 ring-gray-100 cursor-pointer ${
+                      i === index ? "border-4 border-blue-500" : ""
+                    }`}
+                    onClick={() => handleThumbnailClick(i)}
+                  />
+                </div>
+              ))}
+            </div>
+          </section>
         </div>
       </MotionConfig>
-
-      {/* <ImageModal
-        images={allImages}
-        visible={isModalVisible}
-        onClose={handleCloseModal}
-        initialIndex={index}
-      /> */}
     </section>
   );
 }
