ee98984b8866b6a793287a18f2ae46e82950c465
AfricanRealEstates <martinmungaik@gmail.com>
2024-09-07 13:07:28 +0300

    favicon & price format

--- components/properties/new/PropertyCard.tsx
+++ components/properties/new/PropertyCard.tsx
@@ -233,7 +233,7 @@ export default function PropertyCard({
           </div>
           <div className="flex items-center">
             <h2 className="flex items-center gap-x-0.5 text-lg leading-7 font-semibold">
-              <span>{currency == "USD" ? "$" : "KES"}</span>
+              <span>{currency == "USD" ? "$" : "Ksh."}</span>
               {price.toLocaleString()}
 
               {status === "let" ? (
